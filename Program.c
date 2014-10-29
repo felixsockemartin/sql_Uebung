ba8dbf9e52a0d36586cae246e47984e84c737351
felixsockemartin <dragon.felix@web.de>
2014-10-29 19:33:16 +0100

    Code und Statements eingefügt

---
+++ Program.c
@@ -0,0 +1,60 @@
+#include <my_global.h>
+#include <mysql.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+bool send_sql(char* sql)
+{
+	printf("MySQL client version: %s\n", mysql_get_client_info());
+	MYSQL *con = mysql_init(NULL);
+	if (con == NULL) 
+	{
+		fprintf(stderr, "%s\n", mysql_error(con));
+		return false;
+	}
+
+	if (mysql_real_connect(con, "localhost", "fussball", "fcwombat", "Fussball", 0, NULL, 0) == NULL)
+	{
+		fprintf(stderr, "%s\n", mysql_error(con));
+		mysql_close(con);
+		return false;
+	}
+	
+	if(mysql_query(con, sql))
+	{
+		fprintf(stderr, "%s\n", mysql_error(con));
+		mysql_close(con);
+		return false;
+	}
+	
+	MYSQL_RES *result = mysql_store_result(con);
+	int count = mysql_num_fields(result);
+	MYSQL_ROW row;
+	
+	while ((row = mysql_fetch_row(result)))
+	{
+		for(int i = 0; i < count; i++) 
+		{ 
+			printf("%s ", row[i] ? row[i] : "NULL"); 
+
+		} 
+		printf("\n"); 
+	}
+	mysql_close(con);
+	return true;
+}
+
+
+
+int main()
+{
+	char* sql;
+	puts("SQL Statement eingeben:");
+	scanf("%s", &sql);
+	//sql = "SELECT * FROM Mannschaft";
+	send_sql(sql);
+	return 0;
+}
